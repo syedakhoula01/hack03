72f0c8028ab978721b29612cf1e4c8092a1c8504
syeda khoula abid <syeda.khoula.abid234@gmail.com>
2023-10-22 04:57:22 +0500

    taking inputs

--- data/datause.c
+++ data/datause.c
@@ -5,5 +5,13 @@ int main()
     float GB_in_plan , GB_used;
     int day;
 
+    printf("Enter the number of GB in plan of 30 days timeperiod");
+    scanf("%f", &GB_in_plan);
+
+    printf("enter the current day 1-30");
+    scanf("%d",&day);
+
+    printf("enter the total numbers ogf GB used ");
+    scanf("%f", &GB_used);
     
 }
