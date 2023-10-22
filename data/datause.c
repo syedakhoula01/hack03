6415445a4214480c91db706798acd50bb8544312
syeda khoula abid <syeda.khoula.abid234@gmail.com>
2023-10-22 05:05:48 +0500

    checking under and over use

--- data/datause.c
+++ data/datause.c
@@ -20,6 +20,16 @@ int main()
 int remaining_days = 30-day;
 float remaining_data = GB_in_plan - GB_used;
 float avg_daily_allowing = remaining_data / remaining_days;
- 
+
+
+char *status;
+if(avg_use > GB_in_plan/30)
+{
+    status = "over use";
+}
+else if(avg_use< GB_in_plan/30);
+{
+    status = "under use";
+}
 
 }
